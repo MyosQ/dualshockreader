ff9207503336e95b859e536e75477e7cf4d7ecf7
Frej Sundqvist <frej.sundqvist@live.se>
2016-09-07 20:29:49 +0200

    working

--- lamp_controller.c
+++ lamp_controller.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <linux/joystick.h>
 
 int main(){
-   int fd;
-   if((fd = open("/dev/input/js0", O_RDONLY | O_NONBLOCK)) < 0){
-      perror("Couldnt open file");
-      exit(EXIT_FAILURE);
-   }
+	int fd, buttonState = 0, prevButtonState = 0;
+	struct js_event ev;
 
+	if((fd = open("/dev/input/js0", O_RDONLY)) < 0){
+		perror("Couldnt open file");
+	exit(EXIT_FAILURE);
+	}
 
+	while(1){
+		read(fd, &ev, sizeof(ev));
+		if((ev.type & ~JS_EVENT_INIT) == JS_EVENT_BUTTON){
+			if(ev.value)
+				buttonState |= (1 << ev.number);
+			else
+				buttonState &= ~(1 << ev.number);
+		}
+
+		if(ev.value && (buttonState == 2) && (prevButtonState == 0)){
+			printf("EX\n");
+			system("gpio -g mode 24 out");
+			system("gpio -g write 24 1");
+		}
+		if(ev.value && (buttonState == 4) && (prevButtonState == 0)){
+			printf("RING\n");
+			system("gpio -g mode 24 out");
+			system("gpio -g write 24 0");
+		}
+		prevButtonState = buttonState;
+
+	}
 }
