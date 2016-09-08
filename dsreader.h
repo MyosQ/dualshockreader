025adeb4973d6e2b40e4198e8ed649fc8ffba851
Frej Sundqvist <frej.sundqvist@live.se>
2016-09-08 20:57:10 +0200

    function 'tryOpen now implemented'

--- dsreader.h
+++ dsreader.h
@@ -9,6 +9,7 @@
 
 #define MIN_TIME_BETWEEN_CLICKS 0.2
 
+/* Buttons */
 #define DS_SQAURE	0
 #define DS_CROSS	1
 #define DS_RING		2
@@ -24,29 +25,31 @@
 #define DS_PS4		12
 #define DS_TRACKPAD	13
 
-
+/* Axes */
 #define DS_AXES_LEFT_X		0
 #define DS_AXES_LEFT_Y		1
 #define DS_AXES_RIGHT_X		2
 #define DS_AXES_L2		3
 #define DS_AXES_R2		4
 #define DS_AXES_RIGHT_Y		5
-
 #define DS_AXES_ARROWS_LEFT	6
 #define DS_AXES_ARROWS_DOWN	7
-
 // 8,9,10 something with acceleration of the controller //
-
 #define DS_AXES_ROLL		11
 #define DS_AXES_YAW		12
 #define DS_AXES_PITCH		13
-
 #define DS_AXES_TRACKPAD_X	15
 #define DS_AXES_TRACKPAD_Y	16
-
 //--- 17: something with the trackpad, different values when tapped and released ---//
 
+/* Struct for thread arguments */
 struct eventTypes{
 	int* buttons;
 	int* ax;
 };
+
+/*--- Used in lampcontroller ---*/
+#define TRY_OPEN_MAX_TIME 100
+#define TRY_OPEN_SLEEP_TIME 5
+#define TRY_OPEN_MAX_LOOPS TRY_OPEN_MAX_TIME/TRY_OPEN_SLEEP_TIME
+#define DEVICE_PATH "/dev/input/js0"
--- lamp_controller.c
+++ lamp_controller.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <time.h>
-#include <linux/joystick.h>
+#include "dsreader.h"
+
 #define TRY_OPEN_MAX_TIME 100
 #define TRY_OPEN_SLEEP_TIME 5
-#define MAX_LOOPS TRY_OPEN_MAX_TIME/TRY_OPEN_SLEEP_TIME
+#define TRY_OPEN_MAX_LOOPS TRY_OPEN_MAX_TIME/TRY_OPEN_SLEEP_TIME
+#define DEVICE_PATH "/dev/input/js0"
 
 int tryOpen(){
    int fd, a;
-   for(a = 0; a < MAX_LOOPS; a++){
-      if((fd = open("/dev/input/js0", O_RDONLY)) > 0)
+   for(a = 0; a < TRY_OPEN_MAX_LOOPS; a++){
+      if((fd = open(DEVICE_PATH, O_RDONLY)) > 0)
          return fd;
       sleep(TRY_OPEN_SLEEP_TIME);
    }
-
    exit(EXIT_FAILURE);
 }
 
@@ -22,19 +19,15 @@ int main(){
 	int fd, buttonState = 0, prevButtonState = 0;
 	struct js_event ev;
 
-	if((fd = open("/dev/input/js0", O_RDONLY)) < 0){
-		perror("Couldnt open file");
-	exit(EXIT_FAILURE);
-	}
+	/* Try to open the input file */
+	fd = tryOpen();
 
 	while(1){
-		//read(fd, &ev, sizeof(ev));
-      if(read(fd, &ev, sizeof(ev)) < 0){
+		/* Read into event struct */
+		if(read(fd, &ev, sizeof(ev)) < 0)
+			fd = tryOpen();
 
-         if(errno == EBADF)/*File doesnt exist */
-            fd = tryOpen();
-      }
-
+		/* Update buttonState */
 		if((ev.type & ~JS_EVENT_INIT) == JS_EVENT_BUTTON){
 			if(ev.value)
 				buttonState |= (1 << ev.number);
@@ -42,17 +35,17 @@ int main(){
 				buttonState &= ~(1 << ev.number);
 		}
 
-		if(ev.value && (buttonState == 2) && (prevButtonState == 0)){
-			printf("EX\n");
-			system("gpio -g mode 24 out");
-			system("gpio -g write 24 1");
+		/* Check if EX or RING is pressed */
+		if(ev.value && (buttonState == 1 << DS_CROSS) && (prevButtonState == 0)){
+			system("gpio -g mode 24 out && gpio -g write 24 1");
+//			system("gpio -g write 24 1");
 		}
-		if(ev.value && (buttonState == 4) && (prevButtonState == 0)){
-			printf("RING\n");
+		if(ev.value && (buttonState == 1 << DS_RING) && (prevButtonState == 0)){
 			system("gpio -g mode 24 out");
 			system("gpio -g write 24 0");
 		}
-		prevButtonState = buttonState;
 
+		/* Update prevButtonState */
+		prevButtonState = buttonState;
 	}
 }
